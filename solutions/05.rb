1c33ba8b11a41eb4bc1c7ec6867816dcfe85019b
Zdravko Andonov <zdravko.andonov.dev@gmail.com>
2016-01-26 23:20:55 +0200

    Take inner classes out and abstract the Outcome with CommandHelpers#success and #error

--- solutions/05.rb
+++ solutions/05.rb
@@ -1,6 +1,18 @@
 require 'digest/sha1'
 
 class ObjectStore
+  module ObjectStore::CommandHelpers
+    def error(message)
+      ObjectStore::Outcome.new(message, false)
+    end
+
+    def success(message, result = nil)
+      ObjectStore::Outcome.new(message, true, result)
+    end
+  end
+
+  include CommandHelpers
+
   TIME_FORMAT = "%a %b %d %H:%M %Y %z".freeze
 
   attr_accessor :current_branch, :stage, :store
@@ -18,43 +30,10 @@ class ObjectStore
     new_store
   end
 
-  class Outcome
-    attr_reader :message, :result
-
-    def initialize(message, success, result = nil)
-      @message = message
-      @success = success
-      @result = result
-    end
-
-    def success?
-      @success
-    end
-
-    def error?
-      not @success
-    end
-  end
-
-  class Commit
-    attr_reader :date, :message, :hash, :stage
-
-    def initialize(date, message, hash, stage)
-      @date = date
-      @message = message
-      @hash = hash
-      @stage = stage
-    end
-
-    def objects
-      @stage.values
-    end
-  end
-
   def add(name, object)
     @stage[name] = object
     @objects_changed += 1
-    Outcome.new("Added #{name} to stage.", true, object)
+    success("Added #{name} to stage.", object)
   end
 
   def remove(name)
@@ -62,35 +41,38 @@ class ObjectStore
       removed_object = @stage[name]
       @stage.delete(name)
       @objects_changed += 1
-      Outcome.new("Added #{name} for removal.", true, removed_object)
+      success("Added #{name} for removal.", removed_object)
     else
-      Outcome.new("Object #{name} is not committed.", false)
+      error("Object #{name} is not committed.")
     end
   end
 
   def commit(message)
     if @objects_changed == 0
-      Outcome.new("Nothing to commit, working directory clean.", false)
+      error("Nothing to commit, working directory clean.")
     else
       time = Time.now
       time_string = time.strftime(TIME_FORMAT)
       hash = Digest::SHA1.hexdigest(time_string + message)
-      new_commit = Commit.new(time, message, hash, @stage.dup)
-      @store[@current_branch] << new_commit
-      outcome_message = "#{message}\n\t#{@objects_changed} objects changed"
-      @objects_changed = 0
-      Outcome.new(outcome_message, true, new_commit)
+      create_commit(message, time, hash)
     end
+  end
+
+  private def create_commit(message, time, hash)
+    new_commit = Commit.new(time, message, hash, @stage.dup)
+    @store[@current_branch] << new_commit
+    outcome_message = "#{message}\n\t#{@objects_changed} objects changed"
+    @objects_changed = 0
+    success(outcome_message, new_commit)
   end
 
   def get(name)
     last_commit = @store[@current_branch].last
     if last_commit and last_commit.stage.has_key?(name)
-      Outcome.new("Found object #{name}.",
-                  true,
+      success("Found object #{name}.",
                   @store[@current_branch].last.stage[name])
     else
-      Outcome.new("Object #{name} is not committed.", false)
+      error("Object #{name} is not committed.")
     end
   end
 
@@ -100,92 +82,123 @@ class ObjectStore
     if commit_index
       @store[@current_branch].slice!((commit_index + 1)..-1)
       @stage = @store[@current_branch].last.stage
-      Outcome.new("HEAD is now at #{@store[@current_branch].last.hash}.",
-                  true,
+      success("HEAD is now at #{@store[@current_branch].last.hash}.",
                   @store[@current_branch].last)
     else
-      Outcome.new("Commit #{commit_hash} does not exist.", false)
+      error("Commit #{commit_hash} does not exist.")
     end
   end
 
   def head
     if @store[@current_branch].empty?
-      Outcome.new("Branch #{@current_branch} does not have any commits yet.",
-                  false)
+      error("Branch #{@current_branch} does not have any commits yet.")
     else
-      Outcome.new(@store[@current_branch].last.message,
-                  true,
+      success(@store[@current_branch].last.message,
                   @store[@current_branch].last)
     end
   end
 
   def log
     if @store[@current_branch].empty?
-      Outcome.new("Branch #{@current_branch} does not have any commits yet.",
-                  false)
+      error("Branch #{@current_branch} does not have any commits yet.")
     else
       separate_messages = @store[@current_branch].reverse_each.map do |commit|
         "Commit #{commit.hash}\n" +
           "Date: #{commit.date.strftime(TIME_FORMAT)}\n\n" +
             "\t#{commit.message}"
       end
-      Outcome.new(separate_messages.join("\n\n"), true)
-    end
-  end
-
-  class Branch
-    def initialize(repository)
-      @repository = repository
-    end
-
-    def create(branch_name)
-      if @repository.store.has_key?(branch_name.to_sym)
-        Outcome.new("Branch #{branch_name} already exists.", false)
-      else
-        @repository.store[branch_name.to_sym] =
-          @repository.store[@repository.current_branch].dup
-        Outcome.new("Created branch #{branch_name}.", true)
-      end
-    end
-
-    def remove(branch_name)
-      if @repository.store.has_key?(branch_name.to_sym)
-        if @repository.current_branch == branch_name.to_sym
-          Outcome.new("Cannot remove current branch.", false)
-        else
-          @repository.store.delete(branch_name.to_sym)
-          Outcome.new("Removed branch #{branch_name}.", true)
-        end
-      else
-        Outcome.new("Branch #{branch_name} does not exist.", false)
-      end
-    end
-
-    def checkout(branch_name)
-      if @repository.store.has_key?(branch_name.to_sym)
-        @repository.current_branch = branch_name.to_sym
-        checkout_branch = @repository.store[@repository.current_branch]
-        if checkout_branch.empty?
-          @repository.stage = {}
-        else
-          @repository.stage = checkout_branch.last.stage
-        end
-        Outcome.new("Switched to branch #{branch_name}.", true)
-      else
-        Outcome.new("Branch #{branch_name} does not exist.", false)
-      end
-    end
-
-    def list
-      sorted_keys_with_prefix = @repository.store.keys.sort.map do |branch_name|
-        ((branch_name == @repository.current_branch) ? "* " : "  ") +
-          branch_name.to_s
-      end
-      Outcome.new(sorted_keys_with_prefix.join("\n"), true)
+      success(separate_messages.join("\n\n"))
     end
   end
 
   def branch()
     Branch.new(self)
+  end
+end
+
+class ObjectStore::Outcome
+  attr_reader :message, :result
+
+  def initialize(message, success, result = nil)
+    @message = message
+    @success = success
+    @result = result
+  end
+
+  def success?
+    @success
+  end
+
+  def error?
+    not @success
+  end
+end
+
+class ObjectStore::Commit
+  attr_reader :date, :message, :hash, :stage
+
+  def initialize(date, message, hash, stage)
+    @date = date
+    @message = message
+    @hash = hash
+    @stage = stage
+  end
+
+  def objects
+    @stage.values
+  end
+end
+
+class ObjectStore::Branch
+  include ObjectStore::CommandHelpers
+
+  def initialize(repository)
+    @repository = repository
+  end
+
+  def create(branch_name)
+    if @repository.store.has_key?(branch_name.to_sym)
+      error("Branch #{branch_name} already exists.")
+    else
+      @repository.store[branch_name.to_sym] =
+        @repository.store[@repository.current_branch].dup
+      success("Created branch #{branch_name}.")
+    end
+  end
+
+  def remove(branch_name)
+    if @repository.store.has_key?(branch_name.to_sym)
+      if @repository.current_branch == branch_name.to_sym
+        error("Cannot remove current branch.")
+      else
+        @repository.store.delete(branch_name.to_sym)
+        success("Removed branch #{branch_name}.")
+      end
+    else
+      error("Branch #{branch_name} does not exist.")
+    end
+  end
+
+  def checkout(branch_name)
+    if @repository.store.has_key?(branch_name.to_sym)
+      @repository.current_branch = branch_name.to_sym
+      checkout_branch = @repository.store[@repository.current_branch]
+      if checkout_branch.empty?
+        @repository.stage = {}
+      else
+        @repository.stage = checkout_branch.last.stage
+      end
+      success("Switched to branch #{branch_name}.")
+    else
+      error("Branch #{branch_name} does not exist.")
+    end
+  end
+
+  def list
+    sorted_keys_with_prefix = @repository.store.keys.sort.map do |branch_name|
+      ((branch_name == @repository.current_branch) ? "* " : "  ") +
+        branch_name.to_s
+    end
+    success(sorted_keys_with_prefix.join("\n"))
   end
 end
